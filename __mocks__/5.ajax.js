156d671479f21378c2443c670ea723c5e23c6dc1
yu <yuzhongtao238910@126.com>
2024-07-11 15:58:01 +0800

    3

---
+++ __mocks__/5.ajax.js
@@ -0,0 +1,20 @@
+// 同名文件
+export function getList() {
+	return new Promise((resolve, reject) => {
+		resolve([1, 2, 3, 4])
+	})
+}
+
+
+export function getData() {
+	return new Promise((resolve, reject) => {
+		resolve(['香蕉'])
+	})
+}
+
+
+export function getUrl() {
+	return new Promise((resolve, reject) => {
+		resolve(["http://www.baidu.com"])
+	})
+}
--- test/5.ajax.test.js
+++ test/5.ajax.test.js
@@ -1,14 +1,18 @@
 import {
-getList,
-getData,
-getUrl
+	getList,
+	getData,
+	getUrl
 } from "../5.ajax.js"
+
+// 告诉他需要用假的文件了
+// 相当于在这里使用了 __mocks__之中的同名的文件
+jest.mock("../5.ajax.js")
 
 
 describe("测试是否可以正常的获取数据", () => {
 	it("测试 getList", async () => {
 		const list = await getList()
 
-		expect(list).toEqual([1, 2, 3])
+		expect(list).toEqual([1, 2, 3, 4])
 	})
 })
